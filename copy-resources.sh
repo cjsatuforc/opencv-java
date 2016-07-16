7fbb62e096aea90e7a0eea6a7cb9fb6c732a93c9
Paul Jones <paul@pauljones.id.au>
2016-07-16 18:06:40 +1000

    Update windows + linux to 2.4.13

--- copy-resources.sh
+++ copy-resources.sh
@@ -1,6 +1,5 @@
 VERSION=$1
 SHORT_VERSION=`echo $VERSION | tr -d .`
-BASE_DIR=opencv/opencv-$VERSION
 
 echo "Version: $VERSION"
 echo "Short Version: $SHORT_VERSION"
@@ -11,38 +10,32 @@ echo "Cleaning up Java..."
 rm upstream/*.jar
 rm -rf upstream/res/*
 echo "Copying Java..."
-cp $BASE_DIR/target/osx/x86_64/bin/opencv-$SHORT_VERSION.jar upstream
-cp $BASE_DIR/target/osx/x86_64/modules/java/pure_test/.build/build/jar/opencv-test.jar upstream
-cp -r $BASE_DIR/modules/java/common_test/res/* upstream/res
+cp opencv/opencv/build/java/opencv-*.jar upstream
 
 # OSX
 echo "Cleaning up OSX..."
-rm src/main/resources/nu/pattern/opencv/osx/x86_64/cmake.log
 rm src/main/resources/nu/pattern/opencv/osx/x86_64/*.dylib
 echo "Copying OSX..."
-cp $BASE_DIR/target/osx/x86_64/cmake.log src/main/resources/nu/pattern/opencv/osx/x86_64
-cp $BASE_DIR/target/osx/x86_64/lib/libopencv_java$SHORT_VERSION.dylib src/main/resources/nu/pattern/opencv/osx/x86_64
+cp $BASE_DIR/target/osx/x86_64/lib/libopencv_java*.dylib src/main/resources/nu/pattern/opencv/osx/x86_64
 
 # Linux
 echo "Cleaning up Linux x86_32..."
-rm src/main/resources/nu/pattern/opencv/linux/x86_32/cmake.log
 rm src/main/resources/nu/pattern/opencv/linux/x86_32/*.so
 echo "Copying Linux x86_32..."
-cp $BASE_DIR/target/linux/x86_32/cmake.log src/main/resources/nu/pattern/opencv/linux/x86_32
-cp $BASE_DIR/target/linux/x86_32/lib/libopencv_java$SHORT_VERSION.so src/main/resources/nu/pattern/opencv/linux/x86_32
+cp opencv/build-linux32/lib/libopencv_java*.so src/main/resources/nu/pattern/opencv/linux/x86_32
 
 echo "Cleaning up Linux x86_64..."
-rm src/main/resources/nu/pattern/opencv/linux/x86_64/cmake.log
 rm src/main/resources/nu/pattern/opencv/linux/x86_64/*.so
 echo "Copying Linux x86_64..."
-cp $BASE_DIR/target/linux/x86_64/cmake.log src/main/resources/nu/pattern/opencv/linux/x86_64
-cp $BASE_DIR/target/linux/x86_64/lib/libopencv_java$SHORT_VERSION.so src/main/resources/nu/pattern/opencv/linux/x86_64
+cp opencv/build-linux64/lib/libopencv_java*.so src/main/resources/nu/pattern/opencv/linux/x86_64
 
 # Windows
 echo "Cleaning up Windows..."
 rm src/main/resources/nu/pattern/opencv/windows/x86_32/*.dll
 rm src/main/resources/nu/pattern/opencv/windows/x86_64/*.dll
 echo "Copying Windows..."
-cp $BASE_DIR/target/windows/opencv/build/java/x86/opencv_java$SHORT_VERSION.dll src/main/resources/nu/pattern/opencv/windows/x86_32
-cp $BASE_DIR/target/windows/opencv/build/java/x64/opencv_java$SHORT_VERSION.dll src/main/resources/nu/pattern/opencv/windows/x86_64
+cp opencv/opencv/build/java/x86/opencv_java*.dll src/main/resources/nu/pattern/opencv/windows/x86_32
+cp opencv/opencv/build/java/x64/opencv_java*.dll src/main/resources/nu/pattern/opencv/windows/x86_64
+cp opencv/opencv/build/x86/vc12/bin/opencv_ffmpeg* src/main/resources/nu/pattern/opencv/windows/x86_32
+cp opencv/opencv/build/x64/vc12/bin/opencv_ffmpeg* src/main/resources/nu/pattern/opencv/windows/x86_64
 
